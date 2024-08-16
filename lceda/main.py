3a47c5b88aaf7783648b9ba6e041b354702f5201
huang.wenjie <huang.wenjie@deetee.com>
2024-08-16 10:44:39 +0800

    小小更新一波，名称更合理一下

--- lceda/main.py
+++ lceda/main.py
@@ -1,11 +1,13 @@
-from bs4 import BeautifulSoup
-import bs4
 import requests
 import json
 import codecs
-
+import os
+print("本程序用于下载嘉立创3D模型")
+print("作者：kulya97")
+print("发布网址：https://github.com/kulya97/warm_3dmodule")
 path='0819f05c4eef4c71ace90d822a990e87'
-code='C17414'
+code = input("输入元件ID: ")
+#code='C17414'
 #---------------------------------------------------------------#
 has_url = 'https://pro.lceda.cn/api/eda/product/search' #跟距编号搜索hasDevice
 has_formdata = {
@@ -17,12 +19,15 @@ has_formdata = {
 }
 r0 = requests.post(has_url,data = has_formdata)
 json_data = json.loads(r0.text)
-Data_ls = json_data['result']
-Data_ls2 = Data_ls['productList']
-Datas = Data_ls2[0]
-hasDevice = Datas['hasDevice']
+Data_ls1 = json_data['result']
+Data_ls2 = Data_ls1['productList']
+Data_ls3 = Data_ls2[0]
+hasDevice = Data_ls3['hasDevice']
+print('-------------------------------------------------------------------------------------------------------------------------------')
+print('---------------------'+'get hasdevice'+'---------------------')
+print('---------------------'+'this is json data'+'-----------------')
 print(json_data)
-
+print('-------------------------------------------------------------------------------------------------------------------------------')
 #---------------------------------------------------------------#
 url = 'https://pro.lceda.cn/api/devices/searchByIds'
 formdata = {
@@ -30,13 +35,16 @@ formdata = {
 'path':path
 }
 r1 = requests.post(url,data = formdata)
-json1_data = json.loads(r1.text)
-Data1_ls = json1_data['result']
-Data1_ls2 = Data1_ls[0]
-Data1_ls3 = Data1_ls2['attributes']
-Model_id = Data1_ls3['3D Model']
-foot_name = Data1_ls3['Supplier Footprint']
-print(foot_name)
+json_data = json.loads(r1.text)
+Data_ls1 = json_data['result']
+Data_ls2 = Data_ls1[0]
+Data_ls3 = Data_ls2['attributes']
+Model_id = Data_ls3['3D Model']
+print('-------------------------------------------------------------------------------------------------------------------------------')
+print('---------------------'+'get model id'+'---------------------')
+print('---------------------'+'this is json data'+'-----------------')
+print(json_data)
+print('-------------------------------------------------------------------------------------------------------------------------------')
 #---------------------------------------------------------------#
 url2 = 'https://pro.lceda.cn/api/components/searchByIds?forceOnline=1' #请求3d封装名称
 formdata2 = {
@@ -45,20 +53,33 @@ formdata2 = {
 'path':path
 }
 r2 = requests.post(url2,data = formdata2)
-#print(r2.json())
-json1_data = json.loads(r2.text)
-Data2_ls = json1_data['result']
-Data2_ls2 = Data2_ls[0]
-Data2_ls3 = Data2_ls2['dataStr']
-Data2_ls4=json.loads(Data2_ls3)
-Model_id0 = Data2_ls4['model']
-print(json1_data)
+json_data = json.loads(r2.text)
+
+Data_ls1 = json_data['result']
+Data_ls2 = Data_ls1[0]
+Data_ls3 = Data_ls2['dataStr']
+Data_ls4=json.loads(Data_ls3)
+Model_id0 = Data_ls4['model']
+Model_src = Data_ls4['src']
+print('-------------------------------------------------------------------------------------------------------------------------------')
+print('---------------------'+'get hasdevice'+'---------------------')
+print('---------------------'+'this is json data'+'-----------------')
+print(json_data)
+print('-----------------'+'module_src='+Model_src+'-----------------')
+print('-------------------------------------------------------------------------------------------------------------------------------')
 #---------------------------------------------------------------#
 #获取step封装
 download_url="https://modules.lceda.cn/qAxj6KHrDKw4blvCG8QJPs7Y/"+Model_id0
 r3 = requests.get(download_url)
 demo = r3.text
-filename=foot_name+'.step'
+filename=Model_src+'.step'
+print('-------------------------------------------------------------------------------------------------------------------------------')
+print(filename)
+print('-------------------------------------------------------------------------------------------------------------------------------')
 f=codecs.open(filename,'w','utf-8')
 f.write(demo)
+#os.startfile(f)
 f.close()
+current_directory=os.getcwd()
+os.startfile(str(current_directory))
+
